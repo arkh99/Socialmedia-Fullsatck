251a6ad70cd25a57e0c6319a87dedce764e8096d
alireza <arkh919@gmail.com>
2024-08-01 16:31:12 -0400

    authentication done

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -36,3 +36,19 @@ export const register = async (req, res) => {
         res.status(500).json({ error: err.message })
     }
 };
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if (!user) return res.status(400).json({ msg: "User does not exit" });
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({token, user})
+    }
+    catch (err) {
+        res.status(500).json({error: err.message})
+    }
+}
