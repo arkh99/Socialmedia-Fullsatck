289880a6e0eb7de40692d26f6c3b58d0d22771cf
alireza <arkh919@gmail.com>
2024-08-03 23:02:18 -0400

    user routes setup done

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -32,7 +32,31 @@ export const getUserFriends = async (req, res) => {
 
 export const addRemoveFriend = async (req, res) => {
     try {
-        const { id } = req.params;
+        const { id, friendId } = req.params;
         const user = await User.findById(id);
+        const friend = await User.findById(friendId);
+        if (user.friends.includes(friendId)) {
+            user.friends = user.friends.filter((id) => id !== friendId);
+            friend.friends = friend.friends.filter((id) => id !== id);
+        }
+        else {
+            user.friends.push(friendId);
+            friend.friends.push(id)
+        }
+        await user.save();
+        await friendId.save();
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastNAme, occupation, location, picturePath }) => {
+                return { _id, firstName, lastNAme, occupation, location, picturePath }
+            }
+        );
+        res.status(200).json(formattedFriends)
+    }
+    catch (err) {
+        res.status(404).json({ message: err.message })
     }
 }
